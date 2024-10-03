e39b146201beb399afc27d6a0c82241f885f727d
mennaessam1 <mennatullah.ali@student.guc.edu.eg>
2024-10-03 14:52:37 +0300

    requirement81

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,29 +11,6 @@ const sortProductsByRatings = async (req, res) => {
 };
 
 
-// Function to get all products
-const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find().populate('seller', 'username');  // Populate seller username if available
-
-        // If you need to send a public path for pictures stored locally
-            name: product.name,
-            picture: `${req.protocol}://${req.get('host')}/images/${product.picture}`,  // Build image URL dynamically
-            price: product.price,
-            description: product.description,
-            quantity: product.quantity,
-            seller: product.seller ? product.seller.username : 'Admin',  // Handle null seller field
-            ratings: product.ratings,
-            reviews: product.reviews
-        }));
-
-        res.status(200).json(productData);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
-
 module.exports = {
-    getAllProducts,
     sortProductsByRatings,
 };
