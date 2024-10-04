83c2fb20e8a7829a871579b4cbe5cd36ded542bc
seifdiea <seifdiea2003@gmail.com>
2024-10-04 13:35:44 +0300

    Fixed a compile issue

--- routes/touristRoutes.js
+++ routes/touristRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // localhpost:8000/tourist
 // router.get('/register', touristController.tourist_hello);
 router.get('/getallproducts', touristController.getAllProducts);
-router.get('/search', touristController.searchTouristAttractions);
+// router.get('/search', touristController.searchTouristAttractions);
 router.get('/viewProfile/:id', touristController.getTourist);
 router.put('/update/:id',touristController.updateTouristProfile);
 router.get('/sortProducts', touristController.sortProductsByRatings);
