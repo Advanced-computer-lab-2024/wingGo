bcb9f9b38c8ed6a36bed1d0aa3c84a69bcb42321
Malak Hesham <138399875+malak-hesham1@users.noreply.github.com>
2024-11-04 22:49:36 +0200

    sidebarAdded in IT page

--- FrontEnd/Tourigo-Next/src/components/it-view-orig/TourGridRight.tsx
+++ FrontEnd/Tourigo-Next/src/components/it-view-orig/TourGridRight.tsx
@@ -1,32 +1,56 @@
 // TourGridRight.tsx
-
 "use client";
 import React, { useEffect, useState } from "react";
-import SidebarSearchArea from "../shearedComponents/SidebarSearchArea";
-import PaginationWrapper from "../shearedComponents/PaginationWrapper";
 import TourSingleCard from "../common/tourElements/TourSingleCardIt";
 import { Itinerary } from "@/interFace/interFace";
-import { getItinerariesData } from "@/data/it-data";
-import BookingFormModal from "@/elements/modals/BookingFormModal";
+import { getItinerariesData, getFilteredItinerariesData } from "@/data/it-data";
+import ItinerariesContentHeader from "@/elements/itineraries/it-header";
+import ItinerariesSidebarMain from "../itinerariesSidebar/ItinerariesSidebarMain";
+
+interface FilterOptions {
+  budget?: number;
+  date?: string;
+  preferences?: string;
+  language?: string;
+  touristId?: string;
+}
 
 const TourGridRight = () => {
   const [itineraries, setItineraries] = useState<Itinerary[]>([]);
+  const [filters, setFilters] = useState<FilterOptions>({});
+
+  const fetchItineraries = async () => {
+    const data = await getItinerariesData();
+    setItineraries(data);
+  };
+
+  const fetchFilteredItineraries = async (filters: FilterOptions) => {
+    const data = await getFilteredItinerariesData(filters);
+    setItineraries(data); // Update itineraries
+  };
 
   useEffect(() => {
-    const fetchItineraries = async () => {
-      const data = await getItinerariesData();
-      setItineraries(data);
-    };
     fetchItineraries();
   }, []);
 
-  console.log("its "+itineraries.length);
+  const applyFilters = async (newFilters: FilterOptions) => {
+    setFilters(newFilters); // Update filter state
+    await fetchFilteredItineraries(newFilters); // Fetch filtered data
+  };
+
+  useEffect(() => {
+    if (Object.keys(filters).length > 0) {
+      fetchFilteredItineraries(filters); // Apply filters whenever they change
+    }
+  }, [filters]);
+
   return (
     <>
       <section className="bd-tour-grid-area section-space">
         <div className="container">
           <div className="row gy-24">
             <div className="col-xxl-8 col-xl-8 col-lg-7">
+              <ItinerariesContentHeader />
               <div className="row gy-24">
                 {itineraries.map((item) => (
                   <TourSingleCard
@@ -40,13 +64,11 @@ const TourGridRight = () => {
               </div>
             </div>
             <div className="col-xxl-4 col-xl-4 col-lg-5">
-              <SidebarSearchArea placeHolderTextData="Tour Place" />
+              <ItinerariesSidebarMain applyFilters={applyFilters} />
             </div>
           </div>
-          <PaginationWrapper />
         </div>
       </section>
-      <BookingFormModal />
     </>
   );
 };
--- FrontEnd/Tourigo-Next/src/components/it-view-orig/TourGridRightMain.tsx
+++ FrontEnd/Tourigo-Next/src/components/it-view-orig/TourGridRightMain.tsx
@@ -4,10 +4,12 @@ import React from 'react';
 import TourGridRight from './TourGridRight';
 import Breadcrumb from '../common/breadcrumb/BreadCrumb';
 
+
 const TourGridRightMain = () => {
     return (
         <>
             <Breadcrumb titleOne='Upcoming Itineraries' titleTwo='Explore upcoming itineraries' />
+            
             <TourGridRight />
         </>
     );
