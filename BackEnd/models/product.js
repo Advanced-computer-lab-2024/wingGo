cef5b418a8ef5609453b3816230e1aa8bfeba08b
sylviafaris <161754973+sylviafaris@users.noreply.github.com>
2024-12-04 21:58:17 +0200

    reporttttttttttt backend

--- BackEnd/models/product.js
+++ BackEnd/models/product.js
@@ -51,6 +51,12 @@ const productSchema = new Schema({
             review: { type: String, required: true }
         }
     ],
+    sellingDates: [
+        {
+            type: Date,
+            // required: true // if each date in the array must be provided
+        }
+    ],
     discountedPrices: [
         {
             promoCodeId: { type: mongoose.Schema.Types.ObjectId, ref: 'PromoCode' }, // Applied promo code
