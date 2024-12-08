92708fb1760bf12797ba521a6f444b74a3cdbe4c
seifdiea <seifdiea2003@gmail.com>
2024-12-08 23:01:51 +0200

    Flight booking is done with adjustments to cards

--- FrontEnd/Tourigo-Next/src/api/FlightApi.ts
+++ FrontEnd/Tourigo-Next/src/api/FlightApi.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
 const API_URL = 'http://localhost:8000/tourist';
 
@@ -7,6 +9,13 @@ interface FlightSearchParams {
     origin: string;
     destination: string;
     departureDate: string;
+}
+
+interface DecodedToken {
+  username: string;
+  id: string;
+  role: string;
+  mustChangePassword: boolean;
 }
 
 export const searchFlights = async (params: FlightSearchParams): Promise<any> => {
@@ -26,11 +35,26 @@ export const searchFlights = async (params: FlightSearchParams): Promise<any> =>
     }
   };
 
-  export const bookflight = async (params: any): Promise<any> => {
+  export const bookflight = async (flight: any, paymentMethod: String, promoCode: String, adults: Number): Promise<any> => {
     try {
-      console.log('booking flight:', params);
+      
+      const cookie = Cookies.get('token');
+
+      if (!cookie) {
+        throw new Error('No token found');
+      }
+
+      let id = '';
+      if(cookie){
+        const decodedToken = jwtDecode<DecodedToken>(cookie);
+        id = decodedToken.id;
+      }
+
       const response = await axios.post<any>(`${API_URL}/bookFlight/67240ed8c40a7f3005a1d01d`, {
-        flightOffers: params,
+        flightOffers: flight,
+        paymentMethod: paymentMethod,
+        promoCode: promoCode,
+        adults: adults
       });
       console.log('booking data:', response.data);
       return response.data;
