837916ac71426e01732d554b65668cee199c983f
mennaessam1 <mennatullah.ali@student.guc.edu.eg>
2024-12-09 03:21:03 +0200

    updated delete

--- FrontEnd/Tourigo-Next/src/components/tourguide-profile/ProfileDetails.tsx
+++ FrontEnd/Tourigo-Next/src/components/tourguide-profile/ProfileDetails.tsx
@@ -90,6 +90,14 @@ const ProfileDetails: React.FC<ProfileDetailsProps> = ({ id, profileData, logo, 
   };
 
   const handleDelete = async () => {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete your account? This action cannot be undone."
+    );
+  
+    if (!isConfirmed) {
+      return; // Exit if user cancels
+    }
+  
     try {
       const response = await requestAccountDeletion(id);
       console.log("Delete response:", response);
@@ -100,6 +108,7 @@ const ProfileDetails: React.FC<ProfileDetailsProps> = ({ id, profileData, logo, 
       toast.error("Error deleting account. Please try again later.");
     }
   };
+  
 
   return advertiser ? (
     <section className="bd-team-details-area section-space position-relative">
